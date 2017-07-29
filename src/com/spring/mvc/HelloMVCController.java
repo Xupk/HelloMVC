74c275096be3439548d0a173df507e58baa1bd54
Xupk <211450675@qq.com>
2017-07-30 01:42:49 +0800

    Initial commit

---
+++ src/com/spring/mvc/HelloMVCController.java
@@ -0,0 +1,15 @@
+package com.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloMVCController {
+    @RequestMapping(value="/hello",method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+        model.addAttribute("msg", "Spring 3 MVC Hello World");
+        return "hello";
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: xupk
+  Date: 2017/7/30
+  Time: 0:20
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Hello MVC</title>
+  </head>
+  <body>
+  使用IntelliJ IDEA 搭建 Spring MVC 成功！
+  部署成功！
+  运行成功！
+  ${msg}
+  </body>
+</html>
